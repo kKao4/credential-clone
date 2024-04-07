184897a9a963106d2e16ae801cf4437c9ee46846
kKao2 <thanh291100@gmail.com>
2024-04-07 07:44:55 +0700

    commit

--- components/section/MainSection.tsx
+++ components/section/MainSection.tsx
@@ -525,6 +525,7 @@ export default function MainSection({ isMobileDevice }: MainSectionProps) {
             spaceBetween={isMobileDevice ? (width / 100) * 1.5 : 0}
             slidesPerView={isMobileDevice ? 4 : options.twoPage ? 2 : 1}
             slidesPerGroup={isMobileDevice ? (isLandscape ? 1 : bigSwiperSlide.slidesPerGroup) : options.twoPage ? 2 : 1}
+            style={isMobileDevice && isClient ? { height: window.innerHeight } : undefined}
             scrollbar={
               isMobileDevice
                 ? false

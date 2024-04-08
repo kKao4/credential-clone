b694a9a5396fe47c60d01298a46617a53d9af3bd
kKao2 <thanh291100@gmail.com>
2024-04-08 17:31:22 +0700

    commit

--- components/section/MainSection.tsx
+++ components/section/MainSection.tsx
@@ -267,7 +267,7 @@ export default function MainSection({ isMobileDevice }: MainSectionProps) {
     if (isLandscape && !isMobileLandscape) {
       htmlElement.style.transform = "rotate(90deg)";
       htmlElement.style.transformOrigin = "right top";
-      htmlElement.style.width = "93vh";
+      htmlElement.style.width = "92vh";
       htmlElement.style.height = "100vw";
       htmlElement.style.overflow = "hidden"
       htmlElement.style.position = "absolute"
@@ -609,7 +609,7 @@ export default function MainSection({ isMobileDevice }: MainSectionProps) {
             "h-screen": !isLandscape,
             // "w-[100vh] h-[100vw]": isLandscape && !isMobileLandscape
           })}
-            style={isLandscape && !isMobileLandscape ? { width: "93vh", height: "100vw" } : undefined}
+            style={isLandscape && !isMobileLandscape ? { width: "92vh", height: "100vw" } : undefined}
           >
             {slideDataImages.map((item, i) => {
               return (
@@ -632,7 +632,7 @@ export default function MainSection({ isMobileDevice }: MainSectionProps) {
             {/* active slide mobile */}
             {
               isMobileDevice && (
-                <div className="fixed px-4 py-1.5 top-3.5 left-6 rounded-lg font-bold z-40 text-0.875 md:text-[1.5rem] backdrop-blur-md bg-gradient-to-r from-white/60 to-white/40 text-black/80">
+                <div className="fixed px-4 py-1.5 top-3.5 left-3.5 rounded-lg font-bold z-40 text-0.875 md:text-[1.5rem] backdrop-blur-md bg-gradient-to-r from-white/60 to-white/40 text-black/80">
                   {activeSlide} / {slideDataImages.length}
                 </div>
               )
